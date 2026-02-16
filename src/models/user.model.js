26508b86951ff48ddcdaf463761c20eae5c075d4
Jagan Parida <jagan.parida.dev@gmail.com>
2026-02-16 21:01:33 +0530

    add first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: [true, "Username already exists"],
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    unique: [true, "Email already exists"],
+    required: [true, "Enail is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  bio: String,
+  profileImage: {
+    type: String,
+    default:
+      "https://ik.imagekit.io/xscpkjvr9/defaultProfile.webp?updatedAt=1771088943753",
+  },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
