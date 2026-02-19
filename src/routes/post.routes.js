6eef884efe2a5e8e2cc50fc96914a1b726938515
Jagan Parida <jagan.parida.dev@gmail.com>
2026-02-19 19:24:50 +0530

    add the follow model

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,11 +3,34 @@ const postRouter = express.Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const indentifyUser = require("../middlewares/auth.middleware")
+
+/**
+ * POST /api/posts/ [protected]
+ *  - req.body = {caption, image-file}
+ */
 
 postRouter.post(
   "/",
-  upload.single("image"),
+  upload.single("image"), indentifyUser,
   postController.createPostController,
+);
+
+/**
+ * GET /api/posts/ [protected]
+ */
+
+postRouter.get("/", indentifyUser, postController.getPostController);
+
+/**
+ * GET /api/posts/details/:postId
+ * - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+ */
+
+postRouter.get(
+  "/details/:postId",
+  indentifyUser,
+  postController.getPostDetailsController,
 );
 
 module.exports = postRouter;
