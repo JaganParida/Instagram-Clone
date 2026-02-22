0d33f05b5ce97ccd7bc68cf74a26c0f5504dbf2a
Jagan Parida <jagan.parida.dev@gmail.com>
2026-02-22 12:20:29 +0530

    update the full ui with layers

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -100,7 +100,26 @@ async function loginController(req, res) {
   });
 }
 
+/**
+ * get-me controller
+ */
+async function getMeController(req, res) {
+  const userId = req.user.id;
+
+  const user = await userModel.findById(userId);
+
+  res.status(200).json({
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
 module.exports = {
   registerController,
   loginController,
+  getMeController,
 };
